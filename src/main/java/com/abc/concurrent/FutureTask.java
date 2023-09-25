81d93a86185ed141b05c8a487ae0f41145d618f2
LiuJQ9674Git <455298138@qq.com>
2023-09-26 02:56:56 +0800

    add src17 FutureTask

--- src/main/java/com/abc/concurrent/FutureTask.java
+++ src/main/java/com/abc/concurrent/FutureTask.java
@@ -15,7 +15,9 @@ public class FutureTask<V> implements RunnableFuture<V> {
     /**
      * The run state of this task, initially NEW.  The run state
      * transitions to a terminal state only in methods set,
-     * setException, and cancel.  During completion, state may take on
+     * setException, and cancel.
+     *
+     * During completion, state may take on
      * transient values of COMPLETING (while outcome is being set) or
      * INTERRUPTING (only while interrupting the runner to satisfy a
      * cancel(true)). Transitions from these intermediate to final
@@ -64,13 +66,6 @@ public class FutureTask<V> implements RunnableFuture<V> {
         throw new ExecutionException((Throwable)x);
     }
 
-    /**
-     * Creates a {@code FutureTask} that will, upon running, execute the
-     * given {@code Callable}.
-     *
-     * @param  callable the callable task
-     * @throws NullPointerException if the callable is null
-     */
     public FutureTask(Callable<V> callable) {
         if (callable == null)
             throw new NullPointerException();
@@ -78,54 +73,11 @@ public class FutureTask<V> implements RunnableFuture<V> {
         this.state = NEW;       // ensure visibility of callable
     }
 
-    /**
-     * Creates a {@code FutureTask} that will, upon running, execute the
-     * given {@code Runnable}, and arrange that {@code get} will return the
-     * given result on successful completion.
-     *
-     * @param runnable the runnable task
-     * @param result the result to return on successful completion. If
-     * you don't need a particular result, consider using
-     * constructions of the form:
-     * {@code Future<?> f = new FutureTask<Void>(runnable, null)}
-     * @throws NullPointerException if the runnable is null
-     */
     public FutureTask(Runnable runnable, V result) {
         this.callable = Executors.callable(runnable, result);
         this.state = NEW;       // ensure visibility of callable
     }
 
-    public boolean isCancelled() {
-        return state >= CANCELLED;
-    }
-
-    public boolean isDone() {
-        return state != NEW;
-    }
-
-    public boolean cancel(boolean mayInterruptIfRunning) {
-        if (!(state == NEW && STATE.compareAndSet
-                (this, NEW, mayInterruptIfRunning ? INTERRUPTING : CANCELLED)))
-            return false;
-        try {    // in case call to interrupt throws exception
-            if (mayInterruptIfRunning) {
-                try {
-                    Thread t = runner;
-                    if (t != null)
-                        t.interrupt();
-                } finally { // final state
-                    STATE.setRelease(this, INTERRUPTED);
-                }
-            }
-        } finally {
-            finishCompletion();
-        }
-        return true;
-    }
-
-    /**
-     * @throws CancellationException {@inheritDoc}
-     */
     public V get() throws InterruptedException, ExecutionException {
         int s = state;
         if (s <= COMPLETING)
@@ -133,23 +85,73 @@ public class FutureTask<V> implements RunnableFuture<V> {
         return report(s);
     }
 
-    /**
-     * @throws CancellationException {@inheritDoc}
-     */
-    public V get(long timeout, TimeUnit unit)
-            throws InterruptedException, ExecutionException, TimeoutException {
-       return null;
+    private int awaitDone()
+            throws InterruptedException {
+
+        long startTime = 0L;    // Special value 0L means not yet parked
+        WaitNode q = null;
+        boolean queued = false;
+        for (;;) {
+            int s = state;
+            if (s > COMPLETING) {
+                if (q != null)
+                    q.thread = null;
+                return s;
+            }
+            else if (s == COMPLETING) {
+                // We may have already promised (via isDone) that we are done
+                // so never return empty-handed or throw InterruptedException
+                Thread.yield();
+            }else if (Thread.interrupted()) {
+                removeWaiter(q);
+                throw new InterruptedException();
+            }
+            else if (q == null) {
+                q = new WaitNode();
+            }
+            else if (!queued) {
+                queued = WAITERS.weakCompareAndSet(this, q.next = waiters, q);
+            }
+            else {
+                LockSupport.park(this);
+            }
+        }
+    }
+
+    public void run() {
+        if (state != NEW ||
+                !RUNNER.compareAndSet(this, null, Thread.currentThread()))
+            return;
+        try {
+            Callable<V> c = callable;
+            if (c != null && state == NEW) {
+                V result;
+                boolean ran;
+                try {
+                    result = c.call();
+                    ran = true;
+                } catch (Throwable ex) {
+                    result = null;
+                    ran = false;
+                    setException(ex);
+                }
+                if (ran) {
+                    set(result);
+                }
+            }
+        } finally {
+            // runner must be non-null until state is settled to
+            // prevent concurrent calls to run()
+            runner = null;
+            // state must be re-read after nulling runner to prevent
+            // leaked interrupts
+            int s = state;
+            if (s >= INTERRUPTING)
+                handlePossibleCancellationInterrupt(s);
+        }
     }
 
     /**
-     * Protected method invoked when this task transitions to state
-     * {@code isDone} (whether normally or via cancellation). The
-     * default implementation does nothing.  Subclasses may override
-     * this method to invoke completion callbacks or perform
-     * bookkeeping. Note that you can query status inside the
-     * implementation of this method to determine whether this task
-     * has been cancelled.
-     *
      * finishCompletion 调用
      */
     protected void done() { }
@@ -190,40 +192,6 @@ public class FutureTask<V> implements RunnableFuture<V> {
             finishCompletion();
         }
     }
-
-    public void run() {
-        if (state != NEW ||
-                !RUNNER.compareAndSet(this, null, Thread.currentThread()))
-            return;
-        try {
-            Callable<V> c = callable;
-            if (c != null && state == NEW) {
-                V result;
-                boolean ran;
-                try {
-                    result = c.call();
-                    ran = true;
-                } catch (Throwable ex) {
-                    result = null;
-                    ran = false;
-                    setException(ex);
-                }
-                if (ran) {
-                    set(result);
-                }
-            }
-        } finally {
-            // runner must be non-null until state is settled to
-            // prevent concurrent calls to run()
-            runner = null;
-            // state must be re-read after nulling runner to prevent
-            // leaked interrupts
-            int s = state;
-            if (s >= INTERRUPTING)
-                handlePossibleCancellationInterrupt(s);
-        }
-    }
-
 
     /**
      * Ensures that any interrupt from a possible cancel(true) is only
@@ -281,43 +249,6 @@ public class FutureTask<V> implements RunnableFuture<V> {
     }
 
     /**
-     * Awaits completion or aborts on interrupt or timeout.
-     * @return state upon completion or at timeout
-     */
-    private int awaitDone()
-            throws InterruptedException {
-
-        long startTime = 0L;    // Special value 0L means not yet parked
-        WaitNode q = null;
-        boolean queued = false;
-        for (;;) {
-            int s = state;
-            if (s > COMPLETING) {
-                if (q != null)
-                    q.thread = null;
-                return s;
-            }
-            else if (s == COMPLETING) {
-                // We may have already promised (via isDone) that we are done
-                // so never return empty-handed or throw InterruptedException
-                Thread.yield();
-            }else if (Thread.interrupted()) {
-                removeWaiter(q);
-                throw new InterruptedException();
-            }
-            else if (q == null) {
-                q = new WaitNode();
-            }
-            else if (!queued) {
-                queued = WAITERS.weakCompareAndSet(this, q.next = waiters, q);
-            }
-            else {
-                LockSupport.park(this);
-            }
-        }
-    }
-
-    /**
      * Tries to unlink a timed-out or interrupted wait node to avoid
      * accumulating garbage.  Internal nodes are simply unspliced
      * without CAS since it is harmless if they are traversed anyway
@@ -349,6 +280,39 @@ public class FutureTask<V> implements RunnableFuture<V> {
                 break;
             }
         }
+    }
+
+    public boolean isCancelled() {
+        return state >= CANCELLED;
+    }
+
+    public boolean isDone() {
+        return state != NEW;
+    }
+
+    public boolean cancel(boolean mayInterruptIfRunning) {
+        if (!(state == NEW && STATE.compareAndSet
+                (this, NEW, mayInterruptIfRunning ? INTERRUPTING : CANCELLED)))
+            return false;
+        try {    // in case call to interrupt throws exception
+            if (mayInterruptIfRunning) {
+                try {
+                    Thread t = runner;
+                    if (t != null)
+                        t.interrupt();
+                } finally { // final state
+                    STATE.setRelease(this, INTERRUPTED);
+                }
+            }
+        } finally {
+            finishCompletion();
+        }
+        return true;
+    }
+
+    public V get(long timeout, TimeUnit unit)
+            throws InterruptedException, ExecutionException, TimeoutException {
+        return null;
     }
 
     // VarHandle mechanics
