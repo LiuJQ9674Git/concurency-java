62761b9ca264c756f3b3dc4c49d9182a58ef55c6
LiuJQ9674Git <455298138@qq.com>
2023-09-26 01:16:22 +0800

    add synchronousequeue test sample

--- src/main/java/com/abc/queue/SynchronousQueueTest.java
+++ src/main/java/com/abc/queue/SynchronousQueueTest.java
@@ -10,7 +10,14 @@ public class SynchronousQueueTest {
     AtomicInteger atomicInteger = new AtomicInteger(0);
 
     public static void main(String[] args) {
-        new SynchronousQueueTest().handleSynchronousQueue();
+        //new SynchronousQueueTest().handleSynchronousQueue();
+        new SynchronousQueueTest().createSynchronousQueue();
+    }
+
+    public void createSynchronousQueue() {
+        createComsuer();
+        createProduct();
+
     }
 
     public void handleSynchronousQueue() {
@@ -18,19 +25,62 @@ public class SynchronousQueueTest {
         comsuerSynchronousQueue();
     }
 
+    void createProduct() {
+        Thread thread = new Thread(() -> {
+            for (; ; ) {
+                try {
+
+                    int o = atomicInteger.getAndAdd(1);
+                    queue.put(o);
+                    System.out.println(Thread.currentThread().getName()
+                            + "\tDate:\t" + System.currentTimeMillis() + "\t" + "产：\t" + o);
+                    int k = ThreadLocalRandom.current().nextInt(100);
+                    try {
+                        Thread.sleep(k);
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                } catch (InterruptedException e) {
+                    System.err.println(e.getMessage());
+                    throw new RuntimeException(e);
+                }
+            }
+        }, "生产线程:\t");
+
+        thread.start();
+
+    }
+
+    void createComsuer() {
+        Thread thread = new Thread(() -> {
+            try {
+                for (; ; ) {
+                    Object o = queue.take();
+                    System.out.println(Thread.currentThread().getName()
+                            + "\tDate:\t" + System.currentTimeMillis() + "\t消：\t" + o);
+                }
+            } catch (InterruptedException e) {
+                System.err.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+        }, "消费线程:\t");
+        thread.start();
+
+    }
+
     void productSynchronousQueue() {
         for (int i = 0; i < 2; i++) {
             Thread thread = new Thread(() -> {
                 while (true) {
                     try {
-                       int o = atomicInteger.getAndAdd(1);
+                        int o = atomicInteger.getAndAdd(1);
                         if (o > 10) {
                             break;
                         }
                         queue.put(o);
                         System.out.println(Thread.currentThread().getName()
                                 + "\tDate:\t" + System.currentTimeMillis() + "\t" + "产：\t" + o);
-                        int k= ThreadLocalRandom.current().nextInt(100);
+                        int k = ThreadLocalRandom.current().nextInt(100);
                         try {
                             Thread.sleep(k);
                         } catch (InterruptedException e) {
@@ -41,7 +91,7 @@ public class SynchronousQueueTest {
                         throw new RuntimeException(e);
                     }
                 }
-            },"生产线程:\t"+i);
+            }, "生产线程:\t" + i);
             thread.start();
         }
     }
@@ -60,7 +110,7 @@ public class SynchronousQueueTest {
                     System.err.println(e.getMessage());
                     throw new RuntimeException(e);
                 }
-            },"消费线程:\t"+i);
+            }, "消费线程:\t" + i);
             thread.start();
         }
     }
